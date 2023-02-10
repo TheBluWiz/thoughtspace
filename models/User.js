2c4e168748bad37e7ac5d3d93604a18f513f2260
Jamey Wicklund <thebluwiz@TheSanctum.local>
2023-02-09 18:52:35 -0800

    Updates Models

--- models/User.js
+++ models/User.js
@@ -5,10 +5,18 @@ const userSchema = new Schema(
   {
     username: { type: String, required: true, unique: true, trim: true },
     email: { type: String, required: true, unique: true, match: '/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/' },
-    thoughts: [Thought],
-    friends: {
-      type: [userSchema]
-    }
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ]
   }
 );
 
